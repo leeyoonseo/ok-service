313b45018e817cc7f815a14a7dc9358840fa2456
leeyoonseo <okayoon.lee@gmail.com>
2022-02-19 17:32:39 +0900

    update: 이미지 컴포넌트 메인에 추가

--- views/pages/index.vue
+++ views/pages/index.vue
@@ -29,6 +29,24 @@
       </div>
     </section> -->
 
+    <!-- TODO: Custom Mobile Scroll  -->
+    <base-section class="section-posts">
+      <strong class="section-title">Section Title</strong>
+      <div class="contents">
+        <ul class="cards">
+          <li v-for="(itemin, index) in 5" :key="index" class="card-items">
+            <Image-card
+              type="button"
+              src="images/img-cat.jpg"
+              title="'소줏값 오른다'"
+              text="하이트진로 출고가 7.9% 인상, 주정·병뚜껑 등 원부자재값 인상 이유…업계 줄인상 전망"
+              @click="$router.push(`/detail/${index}`)"
+            />
+          </li>
+        </ul>
+      </div>
+    </base-section>
+
     <base-section class="section-guests">
       <strong>Section Title</strong>
       <div class="contents graph"></div>
